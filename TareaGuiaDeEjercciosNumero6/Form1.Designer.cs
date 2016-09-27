d73e68d914e6e600c5dec435a590d8ef86b02b89
Alejandro <Alejandro@ALEJANDROB>
2016-09-27 01:06:49 -0600

    Add project files.

---
+++ TareaGuiaDeEjercciosNumero6/Form1.Designer.cs
@@ -0,0 +1,397 @@
+﻿namespace TareaGuiaDeEjercciosNumero6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textNStudiantes = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textOOP = new System.Windows.Forms.TextBox();
+            this.textMatematicaII = new System.Windows.Forms.TextBox();
+            this.textSociology = new System.Windows.Forms.TextBox();
+            this.textScience = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBoxOOP = new System.Windows.Forms.TextBox();
+            this.textBoxMatematicaII = new System.Windows.Forms.TextBox();
+            this.textBoxSicologia = new System.Windows.Forms.TextBox();
+            this.textBoxScience = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.listboxApprovedUnapproved = new System.Windows.Forms.ListBox();
+            this.listboxAverage = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(354, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textNStudiantes
+            // 
+            this.textNStudiantes.Location = new System.Drawing.Point(337, 38);
+            this.textNStudiantes.Name = "textNStudiantes";
+            this.textNStudiantes.Size = new System.Drawing.Size(100, 20);
+            this.textNStudiantes.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(324, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter Amount of Students";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(25, 239);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Submit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Find The Average on the Subjects";
+            // 
+            // textOOP
+            // 
+            this.textOOP.Location = new System.Drawing.Point(15, 271);
+            this.textOOP.Name = "textOOP";
+            this.textOOP.Size = new System.Drawing.Size(100, 20);
+            this.textOOP.TabIndex = 5;
+            this.textOOP.TextChanged += new System.EventHandler(this.textOOP_TextChanged);
+            // 
+            // textMatematicaII
+            // 
+            this.textMatematicaII.Location = new System.Drawing.Point(15, 303);
+            this.textMatematicaII.Name = "textMatematicaII";
+            this.textMatematicaII.Size = new System.Drawing.Size(100, 20);
+            this.textMatematicaII.TabIndex = 6;
+            this.textMatematicaII.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textSociology
+            // 
+            this.textSociology.Location = new System.Drawing.Point(15, 336);
+            this.textSociology.Name = "textSociology";
+            this.textSociology.Size = new System.Drawing.Size(100, 20);
+            this.textSociology.TabIndex = 7;
+            // 
+            // textScience
+            // 
+            this.textScience.Location = new System.Drawing.Point(15, 370);
+            this.textScience.Name = "textScience";
+            this.textScience.Size = new System.Drawing.Size(100, 20);
+            this.textScience.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(121, 274);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "OOP";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(121, 306);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Math";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(121, 339);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Sociology";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(121, 374);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Sciences";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(665, 124);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Submit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(626, 96);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(162, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Amount of Approve/Unapproved";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(337, 239);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 23;
+            this.button4.Text = "Submit";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(371, 124);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(0, 13);
+            this.label12.TabIndex = 24;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(326, 203);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(126, 13);
+            this.label13.TabIndex = 25;
+            this.label13.Text = "Best Student by Subjects";
+            // 
+            // textBoxOOP
+            // 
+            this.textBoxOOP.Location = new System.Drawing.Point(329, 284);
+            this.textBoxOOP.Name = "textBoxOOP";
+            this.textBoxOOP.Size = new System.Drawing.Size(100, 20);
+            this.textBoxOOP.TabIndex = 26;
+            // 
+            // textBoxMatematicaII
+            // 
+            this.textBoxMatematicaII.Location = new System.Drawing.Point(329, 317);
+            this.textBoxMatematicaII.Name = "textBoxMatematicaII";
+            this.textBoxMatematicaII.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMatematicaII.TabIndex = 27;
+            this.textBoxMatematicaII.TextChanged += new System.EventHandler(this.textBox2_TextChanged_1);
+            // 
+            // textBoxSicologia
+            // 
+            this.textBoxSicologia.Location = new System.Drawing.Point(329, 345);
+            this.textBoxSicologia.Name = "textBoxSicologia";
+            this.textBoxSicologia.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSicologia.TabIndex = 28;
+            // 
+            // textBoxScience
+            // 
+            this.textBoxScience.Location = new System.Drawing.Point(327, 377);
+            this.textBoxScience.Name = "textBoxScience";
+            this.textBoxScience.Size = new System.Drawing.Size(100, 20);
+            this.textBoxScience.TabIndex = 29;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(443, 287);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(30, 13);
+            this.label14.TabIndex = 30;
+            this.label14.Text = "OOP";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(443, 320);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(31, 13);
+            this.label15.TabIndex = 31;
+            this.label15.Text = "Math";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(443, 348);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(53, 13);
+            this.label16.TabIndex = 32;
+            this.label16.Text = "Sociology";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(443, 377);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(51, 13);
+            this.label17.TabIndex = 33;
+            this.label17.Text = "Sciences";
+            // 
+            // listboxApprovedUnapproved
+            // 
+            this.listboxApprovedUnapproved.FormattingEnabled = true;
+            this.listboxApprovedUnapproved.Location = new System.Drawing.Point(629, 154);
+            this.listboxApprovedUnapproved.Name = "listboxApprovedUnapproved";
+            this.listboxApprovedUnapproved.Size = new System.Drawing.Size(175, 108);
+            this.listboxApprovedUnapproved.TabIndex = 34;
+            // 
+            // listboxAverage
+            // 
+            this.listboxAverage.FormattingEnabled = true;
+            this.listboxAverage.Location = new System.Drawing.Point(629, 374);
+            this.listboxAverage.Name = "listboxAverage";
+            this.listboxAverage.Size = new System.Drawing.Size(175, 95);
+            this.listboxAverage.TabIndex = 35;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(678, 345);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 36;
+            this.button5.Text = "Submit";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(693, 317);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(47, 13);
+            this.label10.TabIndex = 42;
+            this.label10.Text = "Average";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 504);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.listboxAverage);
+            this.Controls.Add(this.listboxApprovedUnapproved);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.textBoxScience);
+            this.Controls.Add(this.textBoxSicologia);
+            this.Controls.Add(this.textBoxMatematicaII);
+            this.Controls.Add(this.textBoxOOP);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textScience);
+            this.Controls.Add(this.textSociology);
+            this.Controls.Add(this.textMatematicaII);
+            this.Controls.Add(this.textOOP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textNStudiantes);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textNStudiantes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textOOP;
+        private System.Windows.Forms.TextBox textMatematicaII;
+        private System.Windows.Forms.TextBox textSociology;
+        private System.Windows.Forms.TextBox textScience;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBoxOOP;
+        private System.Windows.Forms.TextBox textBoxMatematicaII;
+        private System.Windows.Forms.TextBox textBoxSicologia;
+        private System.Windows.Forms.TextBox textBoxScience;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.ListBox listboxApprovedUnapproved;
+        private System.Windows.Forms.ListBox listboxAverage;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ TareaGuiaDeEjercciosNumero6/Form1.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//b. arreglo bidimencional de estudiantes y asignaturas
+//   1,1+ 1,2+ 1,3+ 1,4 = res / total asig = promedio por estudiante
+//c. guardar estudent_average[] unidimecional, funcion mayor = 1 valor compararlo con el resto 
+
+namespace TareaGuiaDeEjercciosNumero6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int i;
+        int N;
+
+        public struct Register
+        {
+            public string Name;
+            public int OOP;
+            public int MATH;
+            public int Sociology;
+            public int Science;
+            public double Average;
+
+        }
+         
+
+        Register[] Class;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                textNStudiantes.Focus();
+
+
+                N = int.Parse(textNStudiantes.Text);
+
+                Class = new Register[N];
+
+                i = 0;
+
+                while (i < N)
+                {
+                    Class[i].Name = Microsoft.VisualBasic.Interaction.InputBox("Student Name");
+                    Class[i].OOP = int.Parse(Microsoft.VisualBasic.Interaction.InputBox("What Grade # Student Got On OOP"));
+                    Class[i].MATH = int.Parse(Microsoft.VisualBasic.Interaction.InputBox("What Grade # Student Got On MATH"));
+                    Class[i].Sociology = int.Parse(Microsoft.VisualBasic.Interaction.InputBox("What Grade # Student Got On Sociology"));
+                    Class[i].Science = int.Parse(Microsoft.VisualBasic.Interaction.InputBox("What Grade # Student Got On Science "));
+                    i++;
+                }
+
+
+                textNStudiantes.Enabled = false;
+                button1.Enabled = false;
+            }
+            catch {};
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            int Sum2 = 0;
+            int Sum3 = 0;
+            int Sum4 = 0;
+            int Sum5 = 0;
+            int Average2;
+            int Average3;
+            int Average4;
+            int Average5;
+
+            i = 0;
+            while (i < N)
+            {
+
+                Sum2 = Sum2 + Class[i].OOP;
+                Sum3 = Sum3 + Class[i].MATH;
+                Sum4 = Sum4 + Class[i].Sociology;
+                Sum5 = Sum5 + Class[i].Science;
+                i++;
+
+            }
+            Average2 = Sum2 / N;
+            Average3 = Sum3 / N;
+            Average4 = Sum4 / N;
+            Average5 = Sum5 / N;
+
+            textOOP.Text = Average2.ToString();
+            textMatematicaII.Text = Average3.ToString();
+            textSociology.Text = Average4.ToString();
+            textScience.Text = Average5.ToString();
+
+            textOOP.Enabled = false;
+            textMatematicaII.Enabled = false;
+            textSociology.Enabled = false;
+            textScience.Enabled = false;
+
+        }
+
+         
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            i = 0;
+            int CountA = 0;
+            int CountA1 = 0;
+            int CountA2 = 0;
+            int CountA3 = 0;
+            int CountR = 0;
+            int CountR1 = 0;
+            int CountR2 = 0;
+            int CountR3 = 0;
+            int AvgoopA=0;
+            int avgmathA=0;
+            int avegsocioA=0;
+            int AveScienceA=0;
+            int AvegoopR=0;
+            int AvemathR=0;
+            int AvesocioR=0;
+            int AveScienceR=0;
+
+
+
+            while (i<N)
+            {
+                if (Class[i].OOP >= 60)
+                {
+                    CountA = CountA + 1;
+                }
+                else
+                {
+                    CountR = CountR + 1;
+
+                }
+
+                if(Class[i].MATH>=60)
+                {
+                    CountA1 = CountA1 + 1;
+                }
+                else
+                {
+                    CountR1 = CountR1 + 1;
+                }
+                if(Class[i].Sociology>=60)
+                {
+                    CountA2 = CountA2 + 1;
+                }
+                else
+                {
+                    CountR2 = CountR2 + 1;
+                }
+                if(Class[i].Science>=60)
+                {
+                    CountA3 = CountA3 + 1;
+                }
+                else
+                {
+                    CountR3 = CountR3 + 1;
+                }
+                i++;
+
+
+              }
+            AvgoopA = CountA * 100 / N; ;
+            avgmathA = CountA1 * 100 / N;
+            avegsocioA = CountA2 * 100 / N;
+            AveScienceA = CountA3 * 100 / N;
+
+            AvegoopR = 100 - AvgoopA;
+            AvemathR = 100 - avgmathA;
+            AvesocioR = 100 - avegsocioA;
+            AveScienceR = 100 - AveScienceA;
+
+            listboxApprovedUnapproved.Items.Add("Approved on OPP "+CountA+" "+AvgoopA +"%");
+
+            listboxApprovedUnapproved.Items.Add("Unapproved OOP " + CountR + " " + AvegoopR + "%");
+
+            listboxApprovedUnapproved.Items.Add("Approved Math" + CountA1+" " + avgmathA + "%");
+
+            listboxApprovedUnapproved.Items.Add("Unapproved Math" + CountR1 + " " + AvemathR + "%");
+
+            listboxApprovedUnapproved.Items.Add("Approved Sociology" + CountA2 + " " + avegsocioA + "%");
+
+            listboxApprovedUnapproved.Items.Add("Unapproved Sociology" + CountR2 + " " + AvesocioR + "%");
+
+            listboxApprovedUnapproved.Items.Add("Approved Sciences " + CountA3 + " " + AveScienceA + "%");
+
+            listboxApprovedUnapproved.Items.Add("Unapproved Sciences " + CountR3 + " " + AveScienceR + "%");
+
+
+        }
+
+        private void textBox2_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int Greater;
+            int Greater2;
+            int Greater3;
+            int Greater4;
+            int index=0;
+            int index2=0;
+            int index3=0;
+            int index4=0;
+
+       Greater = Class[0].OOP;
+       Greater2 = Class[0].MATH;
+       Greater3 = Class[0].Sociology;
+       Greater4 = Class[0].Science;
+
+            i = 0;
+
+            while (i < N)
+            {
+
+                if (Class[i].OOP > Greater)
+                {
+                    Greater = Class[i].OOP;
+                    index = i;
+
+                }
+
+
+                if (Class[i].MATH > Greater2)
+                {
+                    Greater2 = Class[i].MATH;
+                    index2 = i;
+
+                }
+ 
+
+                if (Class[i].Sociology > Greater3)
+                {
+                    Greater3 = Class[i].Sociology;
+                    index3 = i;
+                }
+
+               
+                if (Class[i].Science > Greater4)
+                {
+                    Greater4 = Class[i].Science;
+                    index4 = i;
+                }
+                  i++;
+            }
+                textBoxOOP.Text = (Class[index].Name.ToString());
+                textBoxMatematicaII.Text = (Class[index2].Name.ToString());
+                textBoxSicologia.Text = (Class[index3].Name.ToString());
+                textBoxScience.Text = (Class[index4].Name.ToString());
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            i = 0;
+            double best;
+            while (i < N)
+            {
+                Class[i].Average = (Class[i].OOP + Class[i].MATH + Class[i].Sociology + Class[i].Science) / 4;
+
+                listboxAverage.Items.Add("Average of" +' '+Class[i].Name+' '+Class[i].Average +' '+ " %");
+                i++;
+            }
+
+            best = Class[0].Average;
+
+            int Indexx=0;
+
+            i = 1;
+            while (i < N)
+            {
+
+                if (Class[i].Average > best)
+                {
+                    best = Class[i].Average;
+                    Indexx = i;
+                }
+                i++;
+            }
+            listboxAverage.Items.Add("The best student is" + Class[Indexx].Name);
+
+            
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            int i, j;
+            for(i=0; i<N; i++)
+            {
+                for (j=0; j < N; j++)
+                {
+                    
+                }
+            }
+
+
+        }
+
+        private void textOOP_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    } 
+}
